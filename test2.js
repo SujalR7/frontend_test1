905386060cc5780213e6a46e58efc8197049d14d
SujalR7 <sujalrandive7@gmail.com>
2024-10-05 13:05:53 +0530

    Add files via upload

---
+++ test2.js
@@ -0,0 +1,23 @@
+let num=parseInt(prompt("Enter a number"));
+multiply(num);
+function multiply(num)
+    {
+     for(let i=0;i<10;i++)
+     {
+        console.log(num+"*"+i+"="+num*i)
+     }
+}
+let celsius=parseFloat(prompt("Enter a degree"));
+converttemp(celsius)
+function converttemp(celsius){
+    let fahr=(celsius*9/5)+32;
+    console.log(fahr);
+}
+let number=parseFloat(prompt("enter a number"));
+if(number>0)
+    console.log("number is  positive");
+else if(number<0)
+    console.log("number is negative");
+else{
+    console.log("enter a valid number");
+}
